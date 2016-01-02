6c94ed9eeb8ba5755726cc84350ab93a1f1978ce
Michael Wang <michael.icwang@gmail.com>
2016-01-02 17:50:25 +0800

    Add new project.
    This is a web page I made when learning java script array with handlebars template.

---
+++ brave_new_world/js/content.js
@@ -0,0 +1,93 @@
+var content = {
+  chapters: [
+    {
+      chapter: "Chapter One",
+      summary: "A SQUAT grey building of only thirty-four stories. Over the main en-trance the words, CENTRAL LONDON HATCHERY AND CONDITIONING CENTRE, and, in a shield, the World Stat\'s motto, COMMUNITY, IDEN-TITY, STABILITY."
+    },
+
+    {
+      chapter: "Chapter Two",
+      summary: "MR. FOSTER was left in the Decanting Room. The D.H.C. and his students stepped into the nearest lift and were carried up to the fifth floor.<br>INFANT NURSERIES. NEO-PAVLOVIAN CONDITIONING ROOMS, announced the notice board."
+    },
+
+    {
+      chapter: "Chapter Three",
+      summary: "OUTSIDE, in the garden, it was playtime. Naked in the warm June sunshine, six or seven hundred little boys and girls were running with shrill yells over the lawns, or playing ball games, or squatting silently in twos and threes among the flowering shrubs. The roses were in bloom, two nightingales soliloquized in the boskage, a cuckoo was just going out of tune among the lime trees. The air was drowsy with the murmur of bees and helicopters."
+    },
+
+    {
+      chapter: "Chapter Four",
+      summary: "THE LIFT was crowded with men from the Alpha Changing Rooms, and Lenina\'s entry wars greeted by many friendly nods and smiles. She was a popular girl and, at one time or another, had spent a night with almost all of them."
+    },
+
+    {
+      chapter: "Chapter Five",
+      summary: "BY EIGHT O\'CLOCK the light was failing. The loud speaker in the tower of the Stoke Poges Club House began, in a more than human tenor, to announce the closing of the courses. Lenina and Henry abandoned their game and walked back towards the Club. From the grounds of the Internal and External Secretion Trust came the lowing of those thousands of cattle which provided, with their hormones and their milk, the raw materials for the great factory at Farnham Royal."
+    },
+
+    {
+      chapter: "Chapter Six",
+      summary: "ODD, ODD, odd, was Lenina\'s verdict on Bernard Marx. So odd, indeed, that in the course of the succeeding weeks she had wondered more than once whether she shouldn\'t change her mind about the New Mexico holiday, and go instead to the North Pole with Benito Hoover. The trouble was that she knew the North Pole, had been there with George Edzel only last summer, and what was more, found it pretty grim."
+    },
+
+    {
+      chapter: "Chapter Seven",
+      summary: "THE MESA was like a ship becalmed in a strait of lion-coloured dust. The channel wound between precipitous banks, and slanting from one wall to the other across the valley ran a streak of green-the river and its fields. On the prow of that stone ship in the centre of the strait, and seemingly a part of it, a shaped and geometrical outcrop of the naked rock, stood the pueblo of Malpais. Block above block, each story smaller than the one below, the tall houses rose like stepped and amputated pyramids into the blue sky. At their feet lay a straggle of low buildings, a criss-cross of walls; and on three sides the precipices fell sheer into the plain. A few columns of smoke mounted perpendicularly into the windless air and were lost."
+    },
+
+    {
+      chapter: "Chapter Eight",
+      summary: "OUTSIDE, in the dust and among the garbage (there were four dogs now), Bernard and John were walking slowly up and down.<br>\"So hard for me to realize,\" Bernard was saying, \"to reconstruct. As though we were living on different planets, in different centuries. A mother, and all this dirt, and gods, and old age, and disease ...\" He shook his head. \"It\'s almost inconceivable. I shall never understand, unless you explain.\""
+    },
+
+    {
+      chapter: "Chapter Nine",
+      summary: "LENINA felt herself entitled, after this day of queerness and horror, to a complete and absolute holiday. As soon as they got back to the resthouse, she swallowed six half-gramme tablets of soma, lay down on her bed, and within ten minutes had embarked for lunar eternity. It would be eighteen hours at the least before she was in time again."
+    },
+
+    {
+      chapter: "Chapter Ten",
+      summary: "THE HANDS of all the four thousand electric clocks in all the Bloomsbury Centre\'s four thousand rooms marked twenty-seven minutes past two. \"This hive of industry,\" as the Director was fond of calling it, was in the full buzz of work. Every one was busy, everything in ordered motion. Under the microscopes, their long tails furiously lashing, spermatozoa were burrowing head first into eggs; and, fertilized, the eggs were expanding, dividing, or if bokanovskified, budding and breaking up into whole populations of separate embryos. From the Social Predestination Room the escalators went rumbling down into the basement, and there, in the crimson darkness, stewingly warm on their cushion of peritoneum and gorged with blood-surrogate and hormones, the foetuses grew and grew or, poisoned, languished into a stunted Epsilonhood. With a faint hum and rattle the moving racks crawled imperceptibly through the weeks and the recapitulated aeons to where, in the Decanting Room, the newly-unbottled babes uttered their first yell of horror and amazement."
+    },
+
+    {
+      chapter: "Chapter Eleven",
+      summary: "AFTER the scene in the Fertilizing Room, all upper-caste London was wild to see this delicious creature who had fallen on his knees before the Director of Hatcheries and Conditioning-or rather the ex-Director, for the poor man had resigned immediately afterwards and never set foot inside the Centre again-had flopped down and called him (the joke was almost too good to be true!) \"my father.\" Linda, on the contrary, cut no ice; nobody had the smallest desire to see Linda. To say one was a mother-that was past a joke: it was an obscenity."
+    },
+
+    {
+      chapter: "Chapter Twelve",
+      summary: "BERNARD had to shout through the locked door; the Savage would not open.<br>\"But everybody\'s there, waiting for you.\"<br>\"Let them wait,\" came back the muffled voice through the door.<br>\"But you know quite well, John\" (how difficult it is to sound persuasive at the top of one\'s voice!) \"I asked them on purpose to meet you.\"<br>\"You ought to have asked me first whether I wanted to meet them.\"<br>\"But you always came before, John.\""
+    },
+
+    {
+      chapter: "Chapter Thirteen",
+      summary: "HENRY FOSTER loomed up through the twilight of the Embryo Store.<br>\"Like to come to a feely this evening?\"<br>Lenina shook her head without speaking.<br>\"Going out with some one else?\" It interested him to know which of his friends was being had by which other. \"Is it Benito?\" he questioned.<br>She shook her head again.<br>Henry detected the weariness in those purple eyes, the pallor beneath that glaze of lupus, the sadness at the corners of the unsmiling crimson mouth. \"You\'re not feeling ill, are you?\" he asked, a trifle anxiously, afraid that she might be suffering from one of the few remaining infectious diseases."
+    },
+
+    {
+      chapter: "Chapter Fourteen",
+      summary: "THE Park Lane Hospital for the Dying was a sixty-story tower of primrose tiles. As the Savage stepped out of his taxicopter a convoy of gaily-coloured aerial hearses rose whirring from the roof and darted away across the Park, westwards, bound for the Slough Crematorium. At the lift gates the presiding porter gave him the information he required, and he dropped down to Ward 81 (a Galloping Senility ward, the porter explained) on the seventeenth floor."
+    },
+
+    {
+      chapter: "Chapter Fifteen",
+      summary: "THE menial staff of the Park Lane Hospital for the Dying consisted of one hundred and sixty-two Deltas divided into two Bokanovsky Groups of eighty-four red headed female and seventy-eight dark dolychocephalic male twins, respectively. At six, when their working day was over, the two Groups assembled in the vestibule of the Hospital and were served by the Deputy Sub-Bursar with their soma ration."
+    },
+
+    {
+      chapter: "Chapter Sixteen",
+      summary: "THE ROOM into which the three were ushered was the Controller\'s study.<br>\"His fordship will be down in a moment.\" The Gamma butler left them to themselves.<br>Helmholtz laughed aloud.<br>\"It\'s more like a caffeine-solution party than a trial,\" he said, and let himself fall into the most luxurious of the pneumatic arm-chairs. \"Cheer up, Bernard,\" he added, catching sight of his friend\'s green unhappy face. But Bernard would not be cheered; without answering, without even looking at Helmholtz, he went and sat down on the most uncomfortable chair in the room, carefully chosen in the obscure hope of somehow deprecating the wrath of the higher powers."
+    },
+
+    {
+      chapter: "Chapter Seventeen",
+      summary: "\"ART, SCIENCE-you seem to have paid a fairly high price for your happiness,\" said the Savage, when they were alone. \"Anything else?\"<br>\"Well, religion, of course,\" replied the Controller. \"There used to be something called God-before the Nine Years\' War. But I was forgetting; you know all about God, I suppose.\"<br>\"Well ...\" The Savage hesitated. He would have liked to say something about solitude, about night, about the mesa lying pale under the moon, about the precipice, the plunge into shadowy darkness, about death. He would have liked to speak; but there were no words. Not even in Shakespeare."
+    },
+
+    {
+      chapter: "Chapter Eighteen",
+      summary: "THE DOOR was ajar; they entered.<br>\"John!\"<br>From the bathroom came an unpleasant and characteristic sound.<br>\"Is there anything the matter?\" Helmholtz called.<br>There was no answer. The unpleasant sound was repeated, twice; there was silence. Then, with a click the bathroom door opened and, very pale, the Savage emerged."
+    }
+  ]
+};
